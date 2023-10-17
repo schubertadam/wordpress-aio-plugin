9775bdb6539e5db9e7d564734265ff521e91cf1e
Adam Schubert <schubertadam@outlook.com>
2023-10-17 17:02:02 +0200

    Remove unnecessary spaces from aio-wp.php

--- aio-wp/aio-wp.php
+++ aio-wp/aio-wp.php
@@ -4,10 +4,10 @@
  * Date: 2023-10-17
  * Time: 17:00
  *
- *   Plugin Name: AIO WP Extension
- *   Description: A plugin that helps developers to extend the WordPress functionality and use JS frontend
- *   Author: Adam Schubert
- *   Author URI: https://github.com/schubertadam
- *   Version: 1.0
- *   Text Domain: aio-wp
+ * Plugin Name: AIO WP Extension
+ * Description: A plugin that helps developers to extend the WordPress functionality and use JS frontend
+ * Author: Adam Schubert
+ * Author URI: https://github.com/schubertadam
+ * Version: 1.0
+ * Text Domain: aio-wp
  */
