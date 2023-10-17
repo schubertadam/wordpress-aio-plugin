6bffe9cfca94a246c8c212f89d9a8c0516d7250d
Adam Schubert <schubertadam@outlook.com>
2023-10-17 17:01:33 +0200

    Add plugin to docker and WordPress and exclude the folder in .gitignore

---
+++ aio-wp/aio-wp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Adam Schubert.
+ * Date: 2023-10-17
+ * Time: 17:00
+ *
+ *   Plugin Name: AIO WP Extension
+ *   Description: A plugin that helps developers to extend the WordPress functionality and use JS frontend
+ *   Author: Adam Schubert
+ *   Author URI: https://github.com/schubertadam
+ *   Version: 1.0
+ *   Text Domain: aio-wp
+ */
