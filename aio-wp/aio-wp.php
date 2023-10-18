da738f749830c7cb774bb5a7acd980b2ca341a7e
Adam Schubert <schubertadam@outlook.com>
2023-10-18 07:45:25 +0200

    Call init class within aio-wp.php & register hooks

--- aio-wp/aio-wp.php
+++ aio-wp/aio-wp.php
@@ -12,13 +12,31 @@
  * Text Domain: aio-wp
  */
 
+use App\Init;
+
 define('AIO_ROOT', plugin_dir_path(__FILE__));
 define('AIO_URL', plugin_dir_url(__FILE__));
 
 if (file_exists(AIO_ROOT . 'vendor/autoload.php')) {
 	require_once AIO_ROOT . "vendor/autoload.php";
+
+	if (class_exists(Init::class)) {
+		(new Init())->registerServices();
+	} else {
+		add_action('admin_notices', function () {
+			echo '<div class="notice notice-error"><p>Init class does not exist!</p></div>';
+		});
+	}
 } else {
 	add_action('admin_notices', function () {
 		echo '<div class="notice notice-error"><p>AIO WP Extension: please run the composer</p></div>';
 	});
 }
+
+register_activation_hook(__FILE__, function () {
+	flush_rewrite_rules();
+});
+
+register_deactivation_hook(__FILE__, function () {
+	flush_rewrite_rules();
+});
