485b38eda8a0744a0772178fa82c6cc7a6e76c3a
Adam Schubert <schubertadam@outlook.com>
2023-10-19 07:39:35 +0200

    Add interface for register method

---
+++ aio-wp/app/Interfaces/RegisterInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Adam Schubert.
+ * Date: 2023-10-18
+ * Time: 07:14
+ */
+
+namespace App\Interfaces;
+
+interface RegisterInterface {
+	public function register(): void;
+}
