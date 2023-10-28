c71214fdf6af0f9cf8d1c98a4f47a663ada71b7f
Adam Schubert <schubertadam@outlook.com>
2023-10-28 09:25:06 +0200

    Add check if permalink is set to the api version.

--- aio-wp/app/Core/Loader.php
+++ aio-wp/app/Core/Loader.php
@@ -13,7 +13,16 @@ class Loader implements RegisterInterface {
 
 	public function register(): void {
 		// Register API routes
-		iterateTroughFilesAndCallRegisterMethod(AIO_ROOT . "/app/Routes/", "App\\Routes\\");
+		// TODO: add option to enable or disable API
+		$apiEnabled = true;
+
+		if ($apiEnabled) {
+			if (get_option('permalink_structure') !== '/%postname%/') {
+				showErrorMessage("Set permalink to the following: /%postname%/");
+			} else {
+				iterateTroughFilesAndCallRegisterMethod(AIO_ROOT . "/app/Routes/", "App\\Routes\\");
+			}
+		}
 
 		// Register WP specific settings
 		iterateTroughFilesAndCallRegisterMethod(AIO_ROOT . "/app/WPSettings/", "App\\WPSettings\\");
